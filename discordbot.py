a21dbd9a597b0529c34c08a4ecfc82e07ce09c87
FutFut18 <104684779+FutFut18@users.noreply.github.com>
2024-10-11 23:06:15 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -44,10 +44,31 @@ async def on_message(message):
         return
 
     global count
+    additional_info = ""
+
+    if message.reference is not None:
+        try:
+            original_message = await message.channel.fetch_message(message.reference.message_id)
+            original_message_text = original_message.content.replace('\n', '')
+            original_message_text = original_message_text.replace('-#', '')
+            if original_message.author != botDS.user:
+                original_message_author = original_message.author
+            else:
+                original_message_author = ""
+            if original_message_author:
+                additional_info = f" (ответ на: {original_message_author}: \"{original_message_text}\")"
+            else:
+                additional_info = f" (ответ на: \"{original_message_text}\")"
+        except Exception as e:
+            print(f"Error fetching original message: {e}")
+            additional_info = ""
+
     with lock:
         count = (count + 1) % 10
         with open("data1.txt", "w", encoding='utf-8') as data:
-            data.write(f"{count} {message.author.global_name} (@" + str(message.author) + "): \n" + str(message.content))
+            data.write(
+                f"{count} {message.author.global_name} (@" + str(message.author) + f"){additional_info}: \n" + str(message.content)
+            )
 
 thread2 = threading.Thread(target=scan, name="Thread-2", daemon=True)
 thread2.start()

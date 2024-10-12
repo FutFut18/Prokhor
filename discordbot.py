1fb977e29459332788a49769d61d19968ce138ac
FutFut18 <104684779+FutFut18@users.noreply.github.com>
2024-10-12 15:50:50 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,17 +23,29 @@ async def send_message(text):
         await channel.send(text)
 
 
-async def send_image():
-    image_path = "image1.jpg"
+async def send_file(file_path):
     channel = botDS.get_channel(TARGET_CHANNEL_ID)
 
-    if channel and os.path.exists(image_path):
-        with open(image_path, "rb") as img:
-            await channel.send(file=nextcord.File(img, "image1.jpg"))
-            os.remove(image_path)
-        print(f"Изображение '{image_path}' отправлено и удалено")
+    if channel and os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            await channel.send(file=nextcord.File(f, os.path.basename(file_path)))
+            os.remove(file_path)
+        print(f"Файл '{file_path}' отправлен и удален")
     else:
-        print(f"Канал не найден или изображение '{image_path}' не существует")
+        print(f"Канал не найден или файл '{file_path}' не существует")
+
+
+async def send_image_or_file():
+    image_path = "image1.jpg"
+    if os.path.exists(image_path):
+        await send_file(image_path)
+
+    file_extensions = ['zip', 'rar', 'jar', 'txt', 'py', 'java', 'kt', 'pdf', 'exe', 'apk', 'mp3', 'mp4', '7z', 'tar', 'mov', 'webp', 'webm', 'csv', 'json', 'xml', 'html', 'css', 'pptx', 'xlsx', 'docx', 'bmp', 'mkv', 'avi', 'flv', 'wmv', 'sh', 'bat', 'dll', 'rs', 'cpp', 'ogg']
+
+    for ext in file_extensions:
+        file_path = f"{ext}1.{ext}"
+        if os.path.exists(file_path):
+            await send_file(file_path)
 
 
 async def scan_file():
@@ -55,20 +67,17 @@ async def scan_file():
                             await send_message(text)
                             last_message_time = current_time
 
-                    # Проверяем наличие изображения и отправляем, если оно существует
-                    if os.path.exists("image1.jpg"):
-                        await send_image()
+                    await send_image_or_file()
 
-                await asyncio.sleep(1)  # Увеличиваем интервал до 1 сек.
+                await asyncio.sleep(1)
         except Exception as e:
             print(f"Error during scanning: {e}")
-            await asyncio.sleep(1)  # Задержка перед новой попыткой
+            await asyncio.sleep(1)
 
 
 @botDS.event
 async def on_ready():
     print(f"Logged in as {botDS.user}")
-    # Запускаем асинхронную задачу сканирования файла
     botDS.loop.create_task(scan_file())
 
 
@@ -100,14 +109,54 @@ async def on_message(message):
     if message.attachments:
         for attachment in message.attachments:
             if any(attachment.filename.lower().endswith(ext) for ext in ['jpg', 'jpeg', 'png', 'gif']):
-                # Скачиваем изображение
                 image_url = attachment.url
                 image_data = requests.get(image_url).content
-
                 save_path = os.path.join("image.jpg")
-
                 with open(save_path, "wb") as f:
                     f.write(image_data)
+        for attachment in message.attachments:
+            file_mapping = {
+                '.zip': 'zip.zip',
+                '.rar': 'rar.rar',
+                '.jar': 'jar.jar',
+                '.txt': 'txt.txt',
+                '.py': 'py.py',
+                '.java': 'java.java',
+                '.kt': 'kt.kt',
+                '.pdf': 'pdf.pdf',
+                '.exe': 'exe.exe',
+                '.apk': 'apk.apk',
+                '.mp3': 'mp3.mp3',
+                '.mp4': 'mp4.mp4',
+                '.7z': '7z.7z',
+                '.tar': 'tar.tar',
+                '.mov': 'mov.mov',
+                '.webp': 'webp.webp',
+                '.webm': 'webm.webm',
+                '.csv': 'csv.csv',
+                '.json': 'json.json',
+                '.xml': 'xml.xml',
+                '.html': 'html.html',
+                '.css': 'css.css',
+                '.pptx': 'pptx.pptx',
+                '.xlsx': 'xlsx.xlsx',
+                '.docx': 'docx.docx',
+                '.bmp': 'bmp.bmp',
+                '.mkv': 'mkv.mkv',
+                '.avi': 'avi.avi',
+                '.flv': 'flv.flv',
+                '.wmv': 'wmv.wmv',
+                '.sh': 'sh.sh',
+                '.bat': 'bat.bat',
+                '.dll': 'dll.dll',
+                '.rs': 'rs.rs',
+                '.cpp': 'cpp.cpp',
+                '.ogg': 'voice_message.ogg'
+            }
+            for ext, new_name in file_mapping.items():
+                if attachment.filename.endswith(ext):
+                    await attachment.save(new_name)
+                    break
 
     count = (count + 1) % 10
     with open("data1.txt", "w", encoding='utf-8') as data:

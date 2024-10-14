744cb9d463d58d65cab6a8d16947ed2d65410acf
FutFut18 <104684779+FutFut18@users.noreply.github.com>
2024-10-14 17:26:04 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,20 @@
-import subprocess
+import asyncio
 
-subprocess.run(["python", "main.py"])
-subprocess.run(["python", "discordbot.py"])
+async def run_script(script_name):
+    process = None
+    try:
+        process = await asyncio.create_subprocess_exec("python", script_name)
+    except:
+        process = await asyncio.create_subprocess_exec("python3", script_name)
+    return process
+
+async def main():
+    processes = await asyncio.gather(
+        run_script("main.py"),
+        run_script("discordbot.py")
+    )
+    for process in processes:
+        await process.wait()
+    for process in processes:
+        process.terminate()
+asyncio.run(main())

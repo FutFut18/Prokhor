90ff767dbc7664651ff91e580cb94abd1ecb4469
FutFut18 <104684779+FutFut18@users.noreply.github.com>
2024-10-14 17:09:04 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,19 @@
+dstoken = "" #str, токен бота в Discord
+tgtoken = "" #str, токен бота в Telegram
+
+dsid = "" #str, id чата в Discord
+tgid = "" #str, id группы в Telegram
+
+TG_ID_DIVIDER = "\n-# [t^"
+TG_ID_ENDING = "]"
+DS_ID_DIVIDER = "\n [d^"                       #Это не менять, если не знаешь зачем
+DS_ID_DIVIDER_FORMATTING = "\n `[d^"
+DS_ID_ENDING = "]"
+
+'''
+Бот в Telegram должен иметь доступ к сообщениям,
+если это не так, то можно изменить, перейдя в 
+BotFather - Bot settings - group privacy - переключить
+В discord бот должен иметь доступ к данным - "читать
+сообщения"
+'''

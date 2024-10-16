5c4857664e8c3c2898e30b3abe5ea74c87b82c1f
FutFut18 <104684779+FutFut18@users.noreply.github.com>
2024-10-16 21:34:48 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,13 +3,62 @@ tgtoken = "" #str, токен бота в Telegram
 
 dsid = "" #str, id чата в Discord
 tgid = "" #str, id группы в Telegram
+serverid = "" #str, id сервера в Discord
 
 TG_ID_DIVIDER = "\n-# [t^"
 TG_ID_ENDING = "]"
+TG_ID_ENDING_ENDING = "}}"
 DS_ID_DIVIDER = "\n [d^"                       #Это не менять, если не знаешь зачем
 DS_ID_DIVIDER_FORMATTING = "\n `[d^"
 DS_ID_ENDING = "]"
-
+DS_ID_ENDING_ENDING = "}}"
+DS_ID_DIVIDER_DIVIDER = "{{"
+MESSAGE_DIVIDER = "$*_.<?/."
+MESSAGE_ENDING = "ХУЙЪЪХЪ"
+ID_DIVIDER = "ƒʕѮΨ⸮"
+ID_ENDING = "⌘☍℧Ѫ⅋ъ"
+idZ = {
+    '5045228699': '720885937898520666', #futfut19
+}
+webhook_url = ''
+file_mapping = {
+                '.zip': 'zip.zip',
+                '.rar': 'rar.rar',
+                '.jar': 'jar.jar',
+                '.txt': 'txt.txt',
+                '.py': 'py.py',
+                '.java': 'java.java',
+                '.kt': 'kt.kt',
+                '.pdf': 'pdf.pdf',
+                '.exe': 'exe.exe',
+                '.apk': 'apk.apk',
+                '.mp3': 'mp3.mp3',
+                '.mp4': 'mp4.mp4',
+                '.7z': '7z.7z',
+                '.tar': 'tar.tar',
+                '.mov': 'mov.mov',
+                '.webp': 'webp.webp',
+                '.webm': 'webm.webm',
+                '.csv': 'csv.csv',
+                '.json': 'json.json',
+                '.xml': 'xml.xml',
+                '.html': 'html.html',
+                '.css': 'css.css',
+                '.pptx': 'pptx.pptx',
+                '.xlsx': 'xlsx.xlsx',
+                '.docx': 'docx.docx',
+                '.bmp': 'bmp.bmp',
+                '.mkv': 'mkv.mkv',
+                '.avi': 'avi.avi',
+                '.flv': 'flv.flv',
+                '.wmv': 'wmv.wmv',
+                '.sh': 'sh.sh',
+                '.bat': 'bat.bat',
+                '.dll': 'dll.dll',
+                '.rs': 'rs.rs',
+                '.cpp': 'cpp.cpp',
+                '.ogg': 'voice_message.ogg'
+            }
 '''
 Бот в Telegram должен иметь доступ к сообщениям,
 если это не так, то можно изменить, перейдя в 
